2ef946e38a01f19bc5cb3a582eff4dcfe4e27eda
azgiliat <toxa2088@gmail.com>
2020-08-21 20:53:02 +0300

    fix lose this

--- src/user.js
+++ src/user.js
@@ -15,7 +15,7 @@ class User {
 
   writeFile(name, data) {
     return new Promise((resolve, reject) => {
-      fs.appendFile(path.resolve(__dirname, `${name}.json`), JSON.stringify(data), err => {
+      fs.writeFile(path.resolve(__dirname, `${name}.json`), JSON.stringify(data), err => {
         if (err) {
           console.error('Error whilw writing to file')
           reject()
@@ -47,28 +47,30 @@ class User {
         this.myCursus = this.user.cursus_users
         return this.readFile('exams')
           .then(data => {
-            this.exams = data
+            this.exams = data.exams
           }, () => this.exams = [])
       })
   }
 
-
+  async checkExams() {
+    console.log(this)
+    const exams = await axios.getExamsByCursAndCampus(this.myCursus[this.myCursus.length - 1].cursus_id, this.myCampus.id)
+    exams.forEach(exam => {
+      if (this.exams.findIndex(item => item.id === exam.id) === -1) {
+        this.emitter.emit('newExam', exam)
+        console.log('new exam', exam.name)
+      }
+    })
+    this.exams = exams
+    await this.writeFile('exams', {exams})
+  }
 
   async watchEvents() {
     console.log('start watching exams')
     this.emitter.emit('startWatch')
-    setInterval(async () => {
-      console.log('Getting exams again...')
-      const exams = await axios.getExamsByCursAndCampus(this.myCursus[this.myCursus.length - 1].cursus_id, this.myCampus.id)
-      exams.forEach(exam => {
-        if (this.exams.findIndex(item => item.id === exam.id) === -1) {
-          this.emitter.emit('newExam', exam)
-          console.log('new exam', exam.name)
-        }
-      })
-      this.exams = exams
-      await this.writeFile('exams', {exams})
-    }, 60000)
+    console.log('Getting exams again...')
+    await this.checkExams()
+    setInterval(() => this.checkExams(), 60000)
   }
 }
 
